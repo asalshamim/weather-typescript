bf00dae4243f8cf20c507503a360fd01c03db2c4
asalshamim <shamimasal8@gmail.com>
2023-10-17 22:06:18 +0200

    challenge

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-500 via-rose-200 to-green-300 to-yellow-600 h-[100vh] w-full">
+
+      <section className="bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded bg-blue-100">
+        <h1 className="text-4xl font-thin"> Wewather <span className="font-black"> Forecast
+        </span></h1>
+        <p>
+          Enter below a place 
+        </p>
+
+      </section>
+    </main>
+  )
+};
+
+export default App;
